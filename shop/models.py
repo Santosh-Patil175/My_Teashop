3c911c82df6dd1638af6a57ee49bbfb63f06c3fc
Santosh-Patil175 <patilsantosh1175@gmail.com>
2026-02-23 19:13:33 +0530

    updated the models

--- shop/models.py
+++ shop/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -36,8 +35,6 @@ class Wishlist(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     added_at = models.DateTimeField(auto_now_add=True)
 
-from django.db import models
-from django.contrib.auth.models import User
 
 
 class Order(models.Model):
