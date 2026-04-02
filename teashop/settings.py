c0c2acaa04a0f607c0fd79d15512872c02e511a7
Santosh-Patil175 <patilsantosh1175@gmail.com>
2026-04-02 17:00:08 +0530

    Updated the Databse in settings and haven't added the password

--- teashop/settings.py
+++ teashop/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'teashop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myproject_db',
+        'USER': 'root',
+        'PASSWORD': 'xxxxxx@123',
+        'HOST': 'localhost',   
+        'PORT': '3306',
     }
 }
 
